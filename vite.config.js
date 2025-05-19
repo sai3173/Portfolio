8b56a930419b9935041979113794725ec77611fb
sai3173 <saitarun1199@gmail.com>
2025-05-20 00:06:30 +0530

    saved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Portfolio/',
-  plugins: [react()],
+    // git subtree push --prefix dist origin gh-pages
+    base: '/Portfolio/',
+    plugins: [react()]
 })
